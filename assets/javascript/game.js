f4cbd11a522682ee0f51ed9adad96b2106649586
Damian <dramirez@petmate.com>
2018-03-08 21:16:55 -0600

    added html, generate word function, generate

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,53 @@
+var triesLeft = 15;
+
 var incrementWins = function () {
 
 }
 
 var generateWord = function () {
+    var possibleWords = ['red', 'green', 'blue']
+    var chosenWord = possibleWords[Math.floor(Math.random() * possibleWords.length)];
 
+    return chosenWord
+}
+
+var generateBlanks = function () {
+    var chosenWord = generateWord()
+    var blanks = ''
+    for (let i = 0; i < chosenWord.length; i++) {
+        blanks = '_ ' + blanks
+
+    }
+    return blanks
 }
 
 var decrementGuesses = function () {
-
+ triesLeft--;
+ var triesLeftElement = document.getElementById('tries-left');
+ console.log(triesLeft)
+ triesLeftElement.textContent = triesLeft;
 }
 
 var addToGuessesList = function () {
 
 }
 
+document.onkeyup = function (e) {
+    var keyInput = e.key;
+    var blanks = generateBlanks();
+    if (e.key === ' ') {
+        var currentWordElement = document.getElementById('current-word');
+        currentWordElement.textContent = blanks;
+    }
+    //if match 
+        //then show in word, 
+        //if no letters left to guess 
+            //then increment win 
+            //and end game
+    //if not a match 
+        //show in letters guessed
+        //decrease guesses left
+            //if guesses left is less than 0 
+                //then end game
+
+};
